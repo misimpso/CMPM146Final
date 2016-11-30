ac75f01cbf41b91741d075b5756d51de99e90659
Mikey <mikeysimpson4@gmail.com>
2016-11-30 00:52:42 -0800

    Adding more constraints to scratch.lp.

--- ASPCode/scratch.py
+++ ASPCode/scratch.py
@@ -1,8 +1,9 @@
 import subprocess
 import json
 import collections
-import random
 import sys
+import math
+import random
 import pdb
 
 def solve(*args):
@@ -54,40 +55,35 @@ def parse_result(out):
     return dict(preds)
     
 def render_level(design, width, height):
-    '''Given a dict of predicates, return an ASCII-art depiction of the a dungeon.'''
-    sprite = {cell:sprite for (sprite, cell) in design['place'] }
-    print "Sprites: {}".format(sprite)
-    #param = dict(design['param'])
-    #native width, height - 21, 25
-    #native center - (11, 13)
-    glyph = dict(blank='0', vp='21', hp='20', vw='101', hw='100', tlc='107', trc='108', brc='106', blc='105', te='113', be='110', re='112', le='111', isl='120', bt='130', tt='133', rt='132', lt='131', x='140', gd='1', pmsp='4', bsp='10', psp='11', isp='12', csp='13', f='2', sf='3')
-    level_block = ''
+	'''Given a dict of predicates, return an ASCII-art depiction of the a dungeon.'''
+	sprite = {cell:sprite for (sprite, cell) in design['place'] }
+	print '{}\n'.format(design['food'])
+	print design['reach']
+	#native width, height - 21, 25
+	#native center - (11, 13)
+	glyph = dict(blank='0', vp='21', hp='20', vw='101', hw='100', tlc='107', trc='108', brc='106', blc='105', te='113', be='110', re='112', le='111', isl='120', bt='130', tt='133', rt='132', lt='131', x='140', gd='1', pmsp='4', bsp='10', psp='11', isp='12', csp='13', f='2', sf='3')
+	level_block = ''
     
-    for h in range(0, height):
-        for w in range(0, width):
-            if((w, h) in sprite):
-                if w == width - 1:
-                    level_block += glyph[sprite[(w,h)]] + '\n'
-                else:
-                    level_block += glyph[sprite[(w,h)]] + ' '
-            else:
-                if w == width - 1:
-                    level_block += ' 0\n'
-                else:
-                    level_block += '0 '
-    return level_block       
+	for h in range(0, height):
+		for w in range(0, width):
+			if((w, h) in sprite):
+				if w == width - 1:
+					level_block += glyph[sprite[(w,h)]] + '\n'
+				else:
+					level_block += glyph[sprite[(w,h)]] + ' '
+	return level_block       
 
 if __name__ == '__main__':
-    lvlWidth = 11
-    lvlHeight = 11
-    design = solve("./ASPCode/scratch.lp", "-c", "width=%d"%lvlWidth, "-c", "height=%d"%lvlHeight)
-    print design
-    params = '# lvlwidth ' + str(lvlWidth) + '\n# lvlheight ' + str(lvlHeight) + '\n# bgcolor 0 0 0\n# edgecolor 0 0 255\n# fillcolor 0 0 0\n# pelletcolor 255 255 255\n# fruittype 1\n# startleveldata\n'
-    params += render_level(design, lvlWidth, lvlHeight) + '# endleveldata\n\n# sprites'
-    print params
+	print(sys.argv[1])
+	lvlWidth = 11
+	lvlHeight = 11
+	wall_amount = math.floor((lvlWidth * lvlHeight)*(0.43))
+	design = solve("./ASPCode/scratch.lp", "-c", "width=%d"%lvlWidth, "-c", "height=%d"%lvlHeight, "-c", "wall_amount=%d"%wall_amount, '--sign-def=3','--seed='+str(random.randint(0,1<<30)))
+	#print design
+	level = '# lvlwidth ' + str(lvlWidth) + '\n# lvlheight ' + str(lvlHeight) + '\n# bgcolor 0 0 0\n# edgecolor 0 0 255\n# fillcolor 0 0 0\n# pelletcolor 255 255 255\n# fruittype 1\n# startleveldata\n'
+	level += render_level(design, lvlWidth, lvlHeight) + '# endleveldata\n\n# sprites'
+	#print level
     
-    f = open("./pacman-python-master/pacman/res/levels/1.txt", 'w')
-    f.write(params)
-    f.close()
-
-    
+	f = open("./pacman-python-master/pacman/res/levels/1.txt", 'w')
+	f.write(level)
+	f.close()
